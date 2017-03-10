3c11cf27052b5b76e5ba7405e6462e0e680f905a
christylaguardia <christinelaguardia@gmail.com>
2017-03-09 22:06:52 -0800

    Thursdays updates

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# vote-tracker
+# market-analysis-app
+
+##AUTHOR
+Christy La Guardia
+
+##PROJECT
+Code 201 Week 5 Vote Tracking App
+
+##DESCRIPTION
+Create a website that presents three different images for the user to choose from, then records their choice.
+
+##RESOURCES
+https://www.w3schools.com/howto/howto_js_progressbar.asp
+http://stackoverflow.com/questions/24849/execute-script-after-specific-delay-using-javascript
--- js/app.js
+++ js/app.js
@@ -1,14 +1,15 @@
-// 14 images
-// var images = ["bag.jpg", "banana.jpg", "boots.jpg","chair.jpg", "cthulhu.jpg", "dragon.jpg", "pen.jpg", "scissors.jpg", "shark.jpg", "sweep.jpg", "unicorn.jpg", "usb.jpg", "water_can.jpg", "wine_glass.jpg"]
+var products = [];
+var results = [];
+var answered = 0;
 
 // constructor function
 function Product(name, fileName) {
   this.name = name;
   this.fileName = fileName;
+  this.votes = 0;
 }
 
-var products = [];
-// create objects
+// create product objects
 products.push(new Product("Bag", "bag.jpg"));
 products.push(new Product("Banana", "banana.jpg"));
 products.push(new Product("Boots", "boots.jpg"));
@@ -26,46 +27,78 @@ products.push(new Product("Wine Glass", "wine_glass.jpg"));
 
 function showImages() {
   var container = document.getElementById("images-container");
-  // remove exising images
-  var oldImages = document.getElementsByTagName("img");
-  while (oldImages.length > 0) {
-    container.removeChild(oldImages[0]);
+
+  if (answered > 0) {
+    for (var i = 1; i <= 3; i++) {
+      var image = document.getElementById("choice" + i);
+      container.removeChild(image);
+    }
   }
-  // get three new images
+
   var usedIndexes = [];
-  for (var count = 1; count <= 3; count++) {
+
+  for (var i = 1; i <= 3; i++) {
     var randomIndex = Math.floor(Math.random() * products.length);
-    console.log("random index: " + randomIndex);
-    // check if image already used
     while (usedIndexes.indexOf(randomIndex) > -1) {
       randomIndex = Math.floor(Math.random() * products.length);
-      console.log("getting diff index: " + randomIndex);
     }
     var image = document.createElement("img");
+    image.setAttribute("class", "images");
+    image.setAttribute("id","choice" + i);
+
+    if (answered < 15) {
+      image.addEventListener("click", recordClick);
+    }
+
     image.src = "img/" + products[randomIndex].fileName;
     container.appendChild(image);
     usedIndexes.push(randomIndex);
   }
-  makeImagesClickable();
 }
-
-function makeImagesClickable() {
-  var images = document.getElementsByTagName("img");
-  for (var index = 0; index < images.length; index++) {
-    images[index].addEventListener("click", recordClick);
-  }
-}
-
-var results = [];
 
 function recordClick(event) {
+
   var clickedItem = event.target;
+  var itemId = clickedItem.id;
+  // var selected = document.getElementById(itemId);
+  // selected.setAttribute("class", "selected");
+
   var itemSource = clickedItem.src;
   var lastSlash = itemSource.lastIndexOf("/") + 1;
   itemSource = itemSource.substring(lastSlash);
   console.log(itemSource + " was clicked.");
   results.push(itemSource);
-  showImages();
+
+  // var images = document.getElementsByTagName("img");
+  // var pickedImg = images.indexOf(itemSource);
+  // console.log(pickedImg);
+
+
+  // delay(1000);
+  answered++;
+  moveProgressBar();
+  if (answered === 15) {
+    showResults();
+  } else {
+    showImages();
+  }
+}
+
+function moveProgressBar() {
+  var completedBar = document.getElementById("bar");
+  var width = Math.floor((answered / 15) * 100);
+  console.log(width);
+  completedBar.style.width = width + '%';
+  completedBar.innerHTML = answered + " / 15";
+}
+
+function showResults() {
+
+}
+
+function delay(ms) {
+   ms += new Date().getTime();
+   while (new Date() < ms){}
 }
 
 window.addEventListener("load", showImages);
