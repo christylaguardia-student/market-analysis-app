d6d0a76767d79873e0c57ae9799a848ea9e316c8
christylaguardia <christinelaguardia@gmail.com>
2017-03-10 23:09:13 -0800

    Working on strech

--- js/app.js
+++ js/app.js
@@ -27,7 +27,6 @@ products.push(new Product("Wine Glass", "wine_glass.jpg"));
 
 function showImages() {
   var container = document.getElementById("images-container");
-
   // check if 1st set, if not remove previous images
   if (answered > 0) {
     for (var i = 1; i <= 3; i++) {
@@ -45,16 +44,12 @@ function showImages() {
       randomIndex = Math.floor(Math.random() * products.length);
     }
     var productToDisplay = products[randomIndex];
-    // show image
+    // dispaly the image
     var image = document.createElement("img");
     image.setAttribute("caption", productToDisplay.name)
     image.setAttribute("class", "images");
     image.setAttribute("id","choice" + i);
-    // check if this is the last question
-    if (answered < 15) {
-      image.addEventListener("click", recordClick);
-    }
-
+    image.addEventListener("click", recordClick);
     image.src = "img/" + productToDisplay.fileName;
     container.appendChild(image);
     usedIndexes.push(randomIndex);
@@ -71,18 +66,17 @@ function recordClick(event) {
   console.log(itemSource + " was clicked.");
   // add to the results array
   results.push(itemSource);
-
-  // var images = document.getElementsByTagName("img");
-  // var pickedImg = images.indexOf(itemSource);
-  // console.log(pickedImg);
-
-
-  // delay(1000);
   // add to total count
   answered++;
-
+  // update progress bar
   moveProgressBar();
+  // change selected product to thumbs up
+  clickedItem.src = "img/" + "check-icon.png";
+  // wait a few seconds
+  setTimeout(showNextImages, 1000);
+}
 
+function showNextImages() {
   // check if last question
   if (answered === 15) {
     showResults();
@@ -99,31 +93,49 @@ function moveProgressBar() {
 }
 
 function showResults() {
-  // make table
+  // remove elements
+  var oldHeader = document.getElementById("instructions");
+  oldHeader.removeChild(oldHeader.firstChild);
+  var imagesContainer = document.getElementById("images-container");
+  while (imagesContainer.firstChild) {
+    imagesContainer.removeChild(imagesContainer.firstChild);
+  }
+  var progressBar = document.getElementById("progress");
+  while (progressBar.firstChild) {
+    progressBar.removeChild(progressBar.firstChild);
+  }
+
+  // make header
   var resultsElement = document.getElementById("results");
+  var header = document.createElement("h3");
+  header.setAttribute("class", "text");
+  header.textContent = "Your Product Survey Results:";
+  resultsElement.appendChild(header);
+
+  // make table
   var table = document.createElement("table");
   table.setAttribute("class", "data");
-  // make headers
+
+  // make table headers
   var headerRow = document.createElement("tr");
   var headerData2 = document.createElement("th");
   headerData2.setAttribute("colspan","2");
   headerData2.textContent = "Product"
   headerRow.appendChild(headerData2);
   var headerData3 = document.createElement("th");
-  headerData3.textContent = "Your Votes"
+  headerData3.setAttribute("colspan","2");
+  headerData3.textContent = "Votes"
   headerRow.appendChild(headerData3);
   table.appendChild(headerRow)
 
-  // get count of votes for each Product
+  // make row for each product
   var sorted = products.sort();
   for (var i = 1; i < sorted.length; i++) {
     var tableRow = document.createElement("tr");
-
     // add product name
     var tableData2 = document.createElement("td");
     tableData2.textContent = products[i].name;
     tableRow.appendChild(tableData2);
-
     // add thumbnail image
     var tableData3 = document.createElement("td");
     var image = document.createElement("img");
@@ -131,7 +143,6 @@ function showResults() {
     image.src = "img/" + products[i].fileName;
     tableData3.appendChild(image);
     tableRow.appendChild(tableData3);
-
     // get count in results array
     var votes = 0;
     for (var j = 0; j < results.length; j++) {
@@ -140,28 +151,17 @@ function showResults() {
         }
     }
     var tableData4 = document.createElement("td");
+    tableData4.setAttribute("class", "centerAlign");
     tableData4.textContent = votes;
     tableRow.appendChild(tableData4);
+    // get percentage of votes
+    var tableData5 = document.createElement("td");
+    tableData5.setAttribute("class", "centerAlign");
+    tableData5.textContent = Math.round((votes / 15 ) * 100) + "%";
+    tableRow.appendChild(tableData5);
 
     table.appendChild(tableRow);
-
   }
-
-
-
-
-
-  //
-  // for (var i = 1; i < results.length; i++) {
-  //   var tableRow = document.createElement("tr");
-  //   var tableData1 = document.createElement("td");
-  //   tableData1.textContent = i;
-  //   tableRow.appendChild(tableData1);
-  //   var tableData2 = document.createElement("td");
-  //   tableData2.textContent = results[i];
-  //   tableRow.appendChild(tableData2);
-  //   table.appendChild(tableRow);
-  // }
   resultsElement.appendChild(table);
 }
 
